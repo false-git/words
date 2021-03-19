8a3f8cb4bab46e8cd6829bca587f387e9107f7b8
false <false@wizard-limit.net>
2021-03-19 16:41:43 +0900

    何故か残っていたarticleを削除

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_03_19_002109) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "groups", force: :cascade do |t|
     t.integer "index"
     t.string "name"
